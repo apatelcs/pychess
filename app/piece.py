82c15ac95a80efb152c73ec33a64d3e576b6cb5a
Abhay Patel <apatelcs108@gmail.com>
2022-07-29 21:59:23 -0400

    Added exception handling + Piece class raises ValueError if name or side is not a valid value + Added tests to check if valid names/sides do not raise ValueError and if invalid names/sides raise ValueError

--- app/piece.py
+++ app/piece.py
@@ -1,3 +1,6 @@
+ACCEPTED_NAMES = ['P', 'R', 'N', 'B', 'K', 'Q']
+ACCEPTED_SIDES = ['W', 'B']
+
 class Piece:
     '''
     Class for chess pieces
@@ -16,5 +19,9 @@ class Piece:
         name: str -> letter representation of the piece
         side: str -> letter representation of the side the piece belongs to
         '''
+        if name not in ACCEPTED_NAMES:
+            raise ValueError('Nonvalid piece name', name)
+        if side not in ACCEPTED_SIDES:
+            raise ValueError('Nonvalid piece side', side)
         self.name = name
         self.side = side
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -11,3 +11,41 @@ def test_create_piece() -> None:
     sample_piece = Piece(sample_piece_name, sample_piece_side)
 
     assert sample_piece.name == 'P' and sample_piece.side == 'W'
+
+
+def test_valid_names() -> None:
+    '''
+    Tests if valid name does not raise ValueError
+    '''
+    for name in ['P', 'R', 'N', 'B', 'K', 'Q']:
+        try:
+            sample_piece = Piece(name, 'W')
+        except ValueError as exc:
+            assert False, f'{name} raised a ValueError {exc}'
+
+
+def test_name_error() -> None:
+    '''
+    Tests if invalid name raises proper ValueError
+    '''
+    with pytest.raises(ValueError):
+        sample_piece = Piece('H', 'W')
+
+
+def test_valid_sides() -> None:
+    '''
+    Tests if valid side does not raise ValueError
+    '''
+    for side in ['W', 'B']:
+        try:
+            sample_piece = Piece('P', side)
+        except ValueError as exc:
+            assert False, f'{side} raised a ValueError {exc}'
+
+
+def test_side_error() -> None:
+    '''
+    Tests if invalid side raises proper ValueError
+    '''
+    with pytest.raises(ValueError):
+        sample_piece = Piece('P', 'Y')
