5d99b237c2c56affc7f408163225c866c4430d23
Abhay Patel <apatelcs108@gmail.com>
2022-07-30 16:03:20 -0400

    Added piece movement + Added move function to Piece + Added move validation by piece name + Added tests for piece movement

--- app/piece.py
+++ app/piece.py
@@ -10,7 +10,7 @@ class Piece:
     name: str -> letter representation of the piece
     side: str -> letter representation of the side the piece belongs to
     row: int -> row coordinate of the piece
-    col: int -> col coordinate of the piece
+    col: int -> column coordinate of the piece
     '''
     def __init__(self, name: str, side: str, row: int, col: int) -> None:
         '''
@@ -21,8 +21,9 @@ class Piece:
         name: str -> letter representation of the piece
         side: str -> letter representation of the side the piece belongs to
         row: int -> row coordinate of the piece
-        col: int -> col coordinate of the piece
+        col: int -> column coordinate of the piece
         '''
+        # Validity checks
         if not isinstance(name, str):
             raise TypeError('Piece name is not a string', name)
         if not isinstance(side, str):
@@ -39,7 +40,117 @@ class Piece:
             raise ValueError('Nonvalid row value', row)
         if col not in range(8):
             raise ValueError('Nonvalid col value', col)
+        # Assign attribute values
         self.name = name
         self.side = side
         self.row = row
         self.col = col
+
+    
+    def move(self, target_row: int, target_col: int) -> None:
+        '''
+        Updates row and col attributes of current piece to move it according to piece name
+
+        Parameters
+        ----------
+        target_row: int -> new row
+        target_col: int -> new column
+        '''
+        # Check if new position is inside the board
+        if target_row not in range(8) or target_col not in range(8):
+            raise ValueError('Tried to move piece off the board')
+        # Validity checks according to piece name and side
+        allowed_moves = []
+        # Pawns can move forward by one
+        if self.name == 'P':
+            if self.side == 'W':
+                allowed_moves.append((self.row + 1, self.col))
+            else:
+                allowed_moves.append((self.row - 1, self.col))
+        # Rooks can move vertically or horizontally
+        elif self.name == 'R':
+            if target_row != self.row and target_col != self.col:
+                raise ValueError('Tried to move rook diagonally')
+            elif target_row == self.row:
+                allowed_moves += [(self.row, c) for c in range(8)]
+            else:
+                allowed_moves += [(r, self.col) for r in range(8)]
+        # Knights can move in L patterns
+        elif self.name == 'N':
+            valid_knight_moves = [(self.row + 2, self.col - 1), (self.row + 2, self.col + 1),
+                                  (self.row - 2, self.col - 1), (self.row - 2, self.col + 1),
+                                  (self.row + 1, self.col - 2), (self.row - 1, self.col - 2),
+                                  (self.row + 1, self.col + 2), (self.row - 1, self.col + 2)]
+            allowed_moves += valid_knight_moves
+        # Bishops can move diagonally
+        elif self.name == 'B':
+            valid_bishop_moves = [(self.row, self.col)]
+            r = self.row + 1
+            c = self.col - 1
+            while r in range(8) and c in range(8):
+                valid_bishop_moves.append((r, c))
+                r += 1
+                c -= 1
+            r = self.row + 1
+            c = self.col + 1
+            while r in range(8) and c in range(8):
+                valid_bishop_moves.append((r, c))
+                r += 1
+                c += 1
+            r = self.row - 1
+            c = self.col + 1
+            while r in range(8) and c in range(8):
+                valid_bishop_moves.append((r, c))
+                r -= 1
+                c += 1
+            r = self.row - 1
+            c = self.col - 1
+            while r in range(8) and c in range(8):
+                valid_bishop_moves.append((r, c))
+                r -= 1
+                c -= 1
+            allowed_moves += valid_bishop_moves
+        # Kings can move by 1 space diagonally, horizontally, or vertically
+        elif self.name == 'K':
+            valid_king_moves = [(self.row + 1, self.col), (self.row - 1, self.col),
+                                (self.row, self.col + 1), (self.row, self.col - 1),
+                                (self.row + 1, self.col + 1), (self.row + 1, self.col - 1),
+                                (self.row - 1, self.col + 1), (self.row - 1, self.col - 1)]
+            allowed_moves += valid_king_moves
+        # Queens can move diagonally, horizontally, or vertically
+        else:
+            valid_queen_moves = []
+            r = self.row + 1
+            c = self.col - 1
+            while r in range(8) and c in range(8):
+                valid_queen_moves.append((r, c))
+                r += 1
+                c -= 1
+            r = self.row + 1
+            c = self.col + 1
+            while r in range(8) and c in range(8):
+                valid_queen_moves.append((r, c))
+                r += 1
+                c += 1
+            r = self.row - 1
+            c = self.col + 1
+            while r in range(8) and c in range(8):
+                valid_queen_moves.append((r, c))
+                r -= 1
+                c += 1
+            r = self.row - 1
+            c = self.col - 1
+            while r in range(8) and c in range(8):
+                valid_queen_moves.append((r, c))
+                r -= 1
+                c -= 1
+            if target_row == self.row:
+                valid_queen_moves += [(self.row, c) for c in range(8)]
+            elif target_col == self.col:
+                valid_queen_moves += [(r, self.col) for r in range(8)]
+            allowed_moves += valid_queen_moves
+        if (target_row, target_col) not in allowed_moves:
+            raise ValueError(f'Tried moving a {self.name} to an invalid postiion')
+        # If valid, assign new row/col to row/col attributes
+        self.row = target_row
+        self.col = target_col
--- tests/test_piece.py
+++ tests/test_piece.py
@@ -1,3 +1,4 @@
+from random import sample
 import pytest
 from app.piece import Piece
 
@@ -23,7 +24,7 @@ def test_valid_names() -> None:
         try:
             sample_piece = Piece(name, 'W', 0, 0)
         except ValueError as exc:
-            assert False, f'{name} raised a ValueError {exc}'
+            assert False, f'{name} raised a ValueError: {exc}'
 
 
 def test_name_type_error() -> None:
@@ -50,7 +51,7 @@ def test_valid_sides() -> None:
         try:
             sample_piece = Piece('P', side, 0, 0)
         except ValueError as exc:
-            assert False, f'{side} raised a ValueError {exc}'
+            assert False, f'{side} raised a ValueError: {exc}'
 
 
 def test_side_type_error() -> None:
@@ -77,7 +78,7 @@ def test_valid_rows() -> None:
         try:
             sample_piece = Piece('P', 'W', r, 0)
         except ValueError as exc:
-            assert False, f'{r} raised a ValueError {exc}'
+            assert False, f'{r} raised a ValueError: {exc}'
 
 
 def test_row_type_error() -> None:
@@ -104,7 +105,7 @@ def test_valid_cols() -> None:
         try:
             sample_piece = Piece('P', 'W', 0, c)
         except ValueError as exc:
-            assert False, f'{c} raised a ValueError {exc}'
+            assert False, f'{c} raised a ValueError: {exc}'
 
 
 def test_col_type_error() -> None:
@@ -121,3 +122,173 @@ def test_col_value_error() -> None:
     '''
     with pytest.raises(ValueError):
         sample_piece = Piece('P', 'W', 0, 8)
+
+
+def test_move_off_board() -> None:
+    '''
+    Tests if ValueError is raised on trying to move piece off the board
+    '''
+    with pytest.raises(ValueError):
+        sample_piece = Piece('P', 'W', 7, 0)
+        sample_piece.move(8, 0)
+
+
+def test_valid_move_pawn() -> None:
+    '''
+    Tests valid pawn movement
+    '''
+    sample_piece_w = Piece('P', 'W', 1, 0)
+    sample_piece_b = Piece('P', 'B', 6, 0)
+
+    try:
+        sample_piece_w.move(2, 0)
+        sample_piece_b.move(5, 0)
+
+        assert sample_piece_w.row == 2 and sample_piece_w.col == 0 and sample_piece_b.row == 5 and sample_piece_b.col == 0
+    except ValueError as exc:
+        assert False, f'Valid pawn move raised a ValueError: {exc}'
+
+
+def test_invalid_move_pawn_backward_w() -> None:
+    '''
+    Tests if ValueError is raised when pawn moves backward
+    '''
+    sample_piece = Piece('P', 'W', 1, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(0, 0)
+
+
+def test_invalid_move_pawn_far_w() -> None:
+    '''
+    Tests if ValueError is raised when pawn moves too far
+    '''
+    sample_piece = Piece('P', 'W', 1, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(3, 0)
+
+
+def test_invalid_move_pawn_backward_b() -> None:
+    '''
+    Tests if ValueError is raised when pawn moves backward
+    '''
+    sample_piece = Piece('P', 'B', 6, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(7, 0)
+
+
+def test_invalid_move_pawn_far_b() -> None:
+    '''
+    Tests if ValueError is raised when pawn moves too far
+    '''
+    sample_piece = Piece('P', 'B', 6, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(4, 0)
+
+
+def test_valid_move_rook() -> None:
+    '''
+    Tests valid rook movement
+    '''
+    sample_piece = Piece('R', 'W', 0, 0)
+    moves = [(r, 0) for r in range(8)] + [(0, c) for c in range(8)]
+    for mv in moves:
+        try:
+            sample_piece.move(mv[0], mv[1])
+            sample_piece.move(0, 0)
+        except ValueError as exc:
+            assert False, f'Valid rook move raised a ValueError: {exc}'
+
+
+def test_invalid_move_rook() -> None:
+    '''
+    Tests if ValueError is raised when rook moves non-vertically/non-horizontally
+    '''
+    sample_piece = Piece('R', 'W', 0, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(1, 1)
+
+
+def test_valid_move_knight() -> None:
+    '''
+    Tests valid knight movement
+    '''
+    sample_piece = Piece('N', 'W', 2, 2)
+    moves = [(4, 1), (4, 3), (0, 1), (0, 3), (3, 4), (1, 4), (3, 0), (1, 0)]
+    for mv in moves:
+        try:
+            sample_piece.move(mv[0], mv[1])
+            sample_piece.move(2, 2)
+        except ValueError as exc:
+            assert False, f'Valid knight move raised a ValueError: {exc}'
+
+
+def test_invalid_move_knight() -> None:
+    '''
+    Tests if ValueError is raised when knight moves in non-L pattern
+    '''
+    sample_piece = Piece('N', 'W', 2, 2)
+    with pytest.raises(ValueError):
+        sample_piece.move(3, 2)
+
+
+def test_valid_move_bishop() -> None:
+    '''
+    Tests valid bishop movement
+    '''
+    sample_piece = Piece('B', 'W', 0, 0)
+    moves = [(i, i) for i in range(8)]
+    for mv in moves:
+        try:
+            sample_piece.move(mv[0], mv[1])
+            sample_piece.move(0, 0)
+        except ValueError as exc:
+            assert False, f'Valid bishop move raised a ValueError: {exc}'
+
+
+def test_invalid_move_bishop() -> None:
+    '''
+    Tests if ValueError is raised when bishop moves non-diagonally
+    '''
+    sample_piece = Piece('B', 'W', 0, 0)
+    with pytest.raises(ValueError):
+        sample_piece.move(1, 0)
+
+
+def test_valid_move_king() -> None:
+    '''
+    Tests valid king movement
+    '''
+    sample_piece = Piece('K', 'W', 1, 1)
+    moves = [(sample_piece.row - 1, sample_piece.col), (sample_piece.row + 1, sample_piece.col),
+             (sample_piece.row, sample_piece.col - 1), (sample_piece.row, sample_piece.col + 1),
+             (sample_piece.row - 1, sample_piece.col - 1), (sample_piece.row - 1, sample_piece.col + 1),
+             (sample_piece.row + 1, sample_piece.col - 1), (sample_piece.row + 1, sample_piece.col + 1)]
+    for mv in moves:
+        try:
+            sample_piece.move(mv[0], mv[1])
+            sample_piece.move(1, 1)
+        except ValueError as exc:
+            assert False, f'Valid king move raised a ValueError: {exc}'
+
+
+def test_invalid_move_king() -> None:
+    '''
+    Tests if ValueError is raised when king moves non-vertically/non-horizontally/non-diagonally
+    '''
+    sample_piece = Piece('K', 'W', 1, 1)
+    with pytest.raises(ValueError):
+        sample_piece.move(3, 0)
+
+
+def test_valid_move_queen() -> None:
+    '''
+    Tests valid queen movement
+    '''
+    sample_piece = Piece('Q', 'W', 0, 0)
+    moves = [(r, 0) for r in range(8)] + [(0, c) for c in range(8)] + [(i, i) for i in range(8)]
+    for mv in moves:
+        try:
+            sample_piece.move(mv[0], mv[1])
+            sample_piece.move(0, 0)
+        except ValueError as exc:
+            assert False, f'Valid queen move raised a ValueError: {exc}'
